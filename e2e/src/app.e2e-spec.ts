ac68a3d9ee21949a2045002ac9847080fd53a25a
antgerasim <antgerasim@gmail.com>
2018-09-16 19:35:12 +0300

    22% fixed jasmine e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,8 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo()
-    expect(page.getParagraphText()).toEqual('Welcome to local-weather-app!')
+    //expect(page.getParagraphText()).toEqual('Welcome to local-weather-app!')
+    expect(page.getParagraphText()).toEqual('LocalCast Weather')
+
   })
 })
--- src/app/weather/weather.servicefake.ts
+++ src/app/weather/weather.servicefake.ts
@@ -3,18 +3,17 @@ import { IWeatherService } from './weather.service'
 import { Observable, of } from 'rxjs'
 
 export class WeatherServiceFake implements IWeatherService {
+  private fakeWeather: ICurrentWeather = {
+    city: 'Bursa',
+    country: 'TR',
+    date: 1485789600,
+    image: '',
+    temperature: 280.32,
+    description: 'light intensity drizzle',
+  }
 
-    private fakeWeather: ICurrentWeather = {
-        city: 'Bursa',
-        country: 'TR',
-        date: 1485789600,
-        image: '',
-        temperature: 280.32,
-        description: 'light intensity drizzle',
-    }
-
-    getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
-        //throw new Error('Method not implemented.')
-        return of(this.fakeWeather) // of turns fakeweather into observable
-    }
+  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+    //throw new Error('Method not implemented.')
+    return of(this.fakeWeather) // of turns fakeweather into observable
+  }
 }
